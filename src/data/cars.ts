8cc8931a88b9db5b59456cdb5ea44eeedc5c46a3
nadr67557-glitch <nadr67557@gmail.com>
2026-09-12 02:59:20 +0300

    Create cars.ts

---
+++ src/data/cars.ts
@@ -0,0 +1,451 @@
+import { Car } from '../types';
+
+export const cars: Car[] = [
+  {
+    id: 'nissan-patrol-platinum-tt-2026',
+    name: 'Nissan Patrol Platinum Twin Turbo',
+    nameAr: 'نيسان باترول بلاتينيوم توين تيربو',
+    brand: 'Nissan',
+    year: 2026,
+    price: 366000,
+    status: 'بطاقة جمركية',
+    color: 'أزرق داكن معدني',
+    interiorColor: 'تان/جملي',
+    engine: '3.5L V6 Twin Turbo',
+    transmission: '9-speed automatic',
+    drivetrain: 'permanent 4WD',
+    harajUrl: 'https://haraj.com.sa/11186481270?src=android',
+    heroImage: '/images/cars/nissan-patrol-1-ext-1.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/nissan-patrol-1-ext-1.jpg',
+        '/images/cars/nissan-patrol-1-ext-2.jpg',
+        '/images/cars/nissan-patrol-1-ext-3.jpg',
+        '/images/cars/nissan-patrol-1-ext-4.jpg'
+      ],
+      interior: [
+        '/images/cars/nissan-patrol-1-int-1.jpg',
+        '/images/cars/nissan-patrol-1-int-2.jpg',
+        '/images/cars/nissan-patrol-1-int-3.jpg',
+        '/images/cars/nissan-patrol-1-int-4.jpg',
+        '/images/cars/nissan-patrol-1-int-5.jpg',
+        '/images/cars/nissan-patrol-1-int-6.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'nissan-patrol-platinum-white-2026',
+    name: 'Nissan Patrol Platinum',
+    nameAr: 'نيسان باترول بلاتينيوم',
+    brand: 'Nissan',
+    year: 2026,
+    price: 366000,
+    status: 'جديدة',
+    color: 'أبيض/فضي',
+    interiorColor: 'Burgundy',
+    harajUrl: 'https://haraj.com.sa/11187311871?src=android',
+    heroImage: '/images/cars/nissan-patrol-2-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/nissan-patrol-2-ext-1.jpg',
+        '/images/cars/nissan-patrol-2-ext-2.jpg',
+        '/images/cars/nissan-patrol-2-ext-3.jpg'
+      ],
+      interior: [
+        '/images/cars/nissan-patrol-2-int-1.jpg',
+        '/images/cars/nissan-patrol-2-int-2.jpg',
+        '/images/cars/nissan-patrol-2-int-3.jpg',
+        '/images/cars/nissan-patrol-2-int-4.jpg',
+        '/images/cars/nissan-patrol-2-int-5.jpg',
+        '/images/cars/nissan-patrol-2-int-6.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'nissan-patrol-platinum-tan-2026',
+    name: 'Nissan Patrol Platinum',
+    nameAr: 'نيسان باترول بلاتينيوم',
+    brand: 'Nissan',
+    year: 2026,
+    price: 330000,
+    status: 'بطاقة جمركية',
+    color: 'أبيض',
+    interiorColor: 'تان/جملي',
+    agent: 'خليجي',
+    engine: '3.5L V6',
+    engineType: 'naturally aspirated',
+    transmission: '7-speed automatic',
+    drivetrain: 'continuous/permanent 4WD',
+    harajUrl: 'https://haraj.com.sa/11184165351?src=android',
+    heroImage: '/images/cars/nissan-patrol-3-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/nissan-patrol-3-ext-1.jpg',
+        '/images/cars/nissan-patrol-3-ext-2.jpg',
+        '/images/cars/nissan-patrol-3-ext-3.jpg',
+        '/images/cars/nissan-patrol-3-ext-4.jpg'
+      ],
+      interior: [
+        '/images/cars/nissan-patrol-3-int-1.jpg',
+        '/images/cars/nissan-patrol-3-int-2.jpg',
+        '/images/cars/nissan-patrol-3-int-3.jpg',
+        '/images/cars/nissan-patrol-3-int-4.jpg',
+        '/images/cars/nissan-patrol-3-int-5.jpg',
+        '/images/cars/nissan-patrol-3-int-6.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'nissan-patrol-platinum-gray-burgundy-2026',
+    name: 'Nissan Patrol Platinum Twin Turbo',
+    nameAr: 'نيسان باترول بلاتينيوم توين تيربو',
+    brand: 'Nissan',
+    year: 2026,
+    price: 366000,
+    status: 'بطاقة جمركية',
+    color: 'رمادي/أزرق داكن',
+    interiorColor: 'Burgundy',
+    agent: 'خليجي',
+    engine: '3.5L V6 Twin Turbo',
+    cylinders: '6',
+    transmission: 'automatic',
+    harajUrl: 'https://haraj.com.sa/11186481414?src=android',
+    heroImage: '/images/cars/nissan-patrol-4-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/nissan-patrol-4-ext-1.jpg',
+        '/images/cars/nissan-patrol-4-ext-2.jpg',
+        '/images/cars/nissan-patrol-4-ext-3.jpg',
+        '/images/cars/nissan-patrol-4-ext-4.jpg'
+      ],
+      interior: [
+        '/images/cars/nissan-patrol-4-int-1.jpg',
+        '/images/cars/nissan-patrol-4-int-2.jpg',
+        '/images/cars/nissan-patrol-4-int-3.jpg',
+        '/images/cars/nissan-patrol-4-int-4.jpg',
+        '/images/cars/nissan-patrol-4-int-5.jpg',
+        '/images/cars/nissan-patrol-4-int-6.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'nissan-patrol-titanium-black-2026',
+    name: 'Nissan Patrol Titanium',
+    nameAr: 'نيسان باترول تيتانيوم',
+    brand: 'Nissan',
+    year: 2026,
+    price: 345000,
+    status: 'بطاقة جمركية',
+    color: 'أسود',
+    interiorColor: 'تان/جملي',
+    transmission: 'automatic',
+    drivetrain: '4WD',
+    harajUrl: 'https://haraj.com.sa/11187240298?src=android',
+    heroImage: '/images/cars/nissan-patrol-5-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/nissan-patrol-5-ext-1.jpg',
+        '/images/cars/nissan-patrol-5-ext-2.jpg',
+        '/images/cars/nissan-patrol-5-ext-3.jpg',
+        '/images/cars/nissan-patrol-5-ext-4.jpg'
+      ],
+      interior: [
+        '/images/cars/nissan-patrol-5-int-1.jpg',
+        '/images/cars/nissan-patrol-5-int-2.jpg',
+        '/images/cars/nissan-patrol-5-int-3.jpg',
+        '/images/cars/nissan-patrol-5-int-4.jpg',
+        '/images/cars/nissan-patrol-5-int-5.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'nissan-sunny-2026',
+    name: 'Nissan Sunny',
+    nameAr: 'نيسان صني',
+    brand: 'Nissan',
+    year: 2026,
+    price: 48500,
+    status: 'جديدة',
+    color: 'أبيض',
+    interiorColor: 'بيج/رمادي فاتح',
+    engine: '1.5L',
+    cylinders: '4',
+    transmission: 'automatic',
+    harajUrl: 'https://haraj.com.sa/11183490510?src=android',
+    heroImage: '/images/cars/nissan-sunny-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/nissan-sunny-ext-1.jpg',
+        '/images/cars/nissan-sunny-ext-2.jpg',
+        '/images/cars/nissan-sunny-ext-3.jpg',
+        '/images/cars/nissan-sunny-ext-4.jpg'
+      ],
+      interior: [
+        '/images/cars/nissan-sunny-int-1.jpg',
+        '/images/cars/nissan-sunny-int-2.jpg',
+        '/images/cars/nissan-sunny-int-3.jpg',
+        '/images/cars/nissan-sunny-int-4.jpg',
+        '/images/cars/nissan-sunny-int-5.jpg',
+        '/images/cars/nissan-sunny-int-6.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'toyota-land-cruiser-2026',
+    name: 'Toyota Land Cruiser VXR',
+    nameAr: 'تويوتا لاندكروزر VXR',
+    brand: 'Toyota',
+    year: 2026,
+    price: 309000,
+    status: 'جديدة',
+    color: 'أبيض',
+    interiorColor: 'بيج',
+    engine: '4.0L',
+    cylinders: '6',
+    engineType: 'naturally aspirated',
+    transmission: '6-speed automatic',
+    drivetrain: '4WD',
+    harajUrl: 'https://haraj.com.sa/11187258579?src=android',
+    heroImage: '/images/cars/toyota-lc-1-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/toyota-lc-1-ext-1.jpg',
+        '/images/cars/toyota-lc-1-ext-2.jpg',
+        '/images/cars/toyota-lc-1-ext-3.jpg',
+        '/images/cars/toyota-lc-1-ext-4.jpg'
+      ],
+      interior: [
+        '/images/cars/toyota-lc-1-int-1.jpg',
+        '/images/cars/toyota-lc-1-int-2.jpg',
+        '/images/cars/toyota-lc-1-int-3.jpg',
+        '/images/cars/toyota-lc-1-int-4.jpg',
+        '/images/cars/toyota-lc-1-int-5.jpg',
+        '/images/cars/toyota-lc-1-int-6.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'toyota-land-cruiser-gxr-l1-2026',
+    name: 'Toyota Land Cruiser GXR L1',
+    nameAr: 'تويوتا لاندكروزر GXR L1',
+    brand: 'Toyota',
+    year: 2026,
+    price: 255000,
+    status: 'جديدة',
+    color: 'أسود',
+    interiorColor: 'بيج',
+    engine: '4.0L',
+    cylinders: '6',
+    engineType: 'naturally aspirated',
+    transmission: '6-speed automatic',
+    drivetrain: '4WD',
+    harajUrl: 'https://haraj.com.sa/11187936018?src=android',
+    heroImage: '/images/cars/toyota-lc-2-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/toyota-lc-2-ext-1.jpg',
+        '/images/cars/toyota-lc-2-ext-2.jpg',
+        '/images/cars/toyota-lc-2-ext-3.jpg',
+        '/images/cars/toyota-lc-2-ext-4.jpg',
+        '/images/cars/toyota-lc-2-ext-5.jpg'
+      ],
+      interior: [
+        '/images/cars/toyota-lc-2-int-1.jpg',
+        '/images/cars/toyota-lc-2-int-2.jpg',
+        '/images/cars/toyota-lc-2-int-3.jpg',
+        '/images/cars/toyota-lc-2-int-4.jpg',
+        '/images/cars/toyota-lc-2-int-5.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'toyota-land-cruiser-l5-al-sayer-2026',
+    name: 'Toyota Land Cruiser L5 Al-Sayer',
+    nameAr: 'تويوتا لاندكروزر L5',
+    brand: 'Toyota',
+    year: 2026,
+    price: 351000,
+    status: 'بطاقة جمركية',
+    color: 'أبيض',
+    interiorColor: 'بيج',
+    agent: 'الساير',
+    warranty: 'يشمل ضمان عبداللطيف',
+    description: 'وارد الساير',
+    engine: '3.5L V6 Twin Turbo',
+    cylinders: '6',
+    engineType: 'twin turbo',
+    transmission: '10-speed automatic',
+    drivetrain: '4WD',
+    harajUrl: 'https://haraj.com.sa/11187393108?src=android',
+    heroImage: '/images/cars/toyota-lc-3-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/toyota-lc-3-ext-1.jpg',
+        '/images/cars/toyota-lc-3-ext-2.jpg',
+        '/images/cars/toyota-lc-3-ext-3.jpg'
+      ],
+      interior: [
+        '/images/cars/toyota-lc-3-int-1.jpg',
+        '/images/cars/toyota-lc-3-int-2.jpg',
+        '/images/cars/toyota-lc-3-int-3.jpg',
+        '/images/cars/toyota-lc-3-int-4.jpg',
+        '/images/cars/toyota-lc-3-int-5.jpg',
+        '/images/cars/toyota-lc-3-int-6.jpg',
+        '/images/cars/toyota-lc-3-int-7.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'toyota-land-cruiser-2026-272',
+    name: 'Toyota Land Cruiser',
+    nameAr: 'تويوتا لاندكروزر',
+    brand: 'Toyota',
+    year: 2026,
+    price: 272000,
+    status: 'جديدة',
+    color: 'أبيض',
+    engine: '3.5L V6 Twin Turbo',
+    cylinders: '6 V6',
+    engineType: 'twin turbo',
+    horsepower: '409 hp',
+    torque: '650 Nm',
+    transmission: '10-speed automatic',
+    drivetrain: 'continuous/permanent 4WD',
+    fuelTankCapacity: '110 L',
+    fuelEfficiency: '10.1 km/L',
+    harajUrl: 'https://haraj.com.sa/11187937731?src=android',
+    heroImage: '/images/cars/toyota-lc-4-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/toyota-lc-4-ext-1.jpg',
+        '/images/cars/toyota-lc-4-ext-2.jpg',
+        '/images/cars/toyota-lc-4-ext-3.jpg',
+        '/images/cars/toyota-lc-4-ext-4.jpg'
+      ],
+      interior: [
+        '/images/cars/toyota-lc-4-int-1.jpg',
+        '/images/cars/toyota-lc-4-int-2.jpg',
+        '/images/cars/toyota-lc-4-int-3.jpg',
+        '/images/cars/toyota-lc-4-int-4.jpg',
+        '/images/cars/toyota-lc-4-int-5.jpg',
+        '/images/cars/toyota-lc-4-int-6.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'lexus-lx600-fsport-2026',
+    name: 'Lexus LX600 F Sport',
+    nameAr: 'لكزس LX600 F Sport',
+    brand: 'Lexus',
+    year: 2026,
+    price: 598000,
+    status: 'available',
+    color: 'أسود ملكي',
+    engine: '3.5L V6 Twin Turbo',
+    transmission: '10-speed automatic',
+    drivetrain: 'permanent 4WD',
+    harajUrl: 'https://haraj.com.sa/11185123598?src=android',
+    heroImage: '/images/cars/lexus-lx600-hero.jpg',
+    gallery: {
+      exterior: ['/images/cars/lexus-lx600-ext-1.jpg'],
+      interior: ['/images/cars/lexus-lx600-int-1.jpg'],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'lexus-es250-aa-2025',
+    name: 'Lexus ES250 AA',
+    nameAr: 'لكزس ES250 AA',
+    brand: 'Lexus',
+    year: 2025,
+    price: 189000,
+    status: 'جديدة',
+    color: 'أبيض',
+    agent: 'شركة عبداللطيف جميل',
+    warranty: '3 years or 100,000 km, whichever comes first',
+    engine: '2.5L',
+    cylinders: '4',
+    transmission: '8-speed automatic',
+    drivetrain: 'front-wheel drive',
+    freeServices: ['tinting', 'wash', 'nano for glass', '50% discount on additional services'],
+    harajUrl: 'https://haraj.com.sa/11187525774?src=android',
+    heroImage: '/images/cars/lexus-es250-hero.jpg',
+    gallery: {
+      exterior: [
+        '/images/cars/lexus-es250-ext-1.jpg',
+        '/images/cars/lexus-es250-ext-2.jpg',
+        '/images/cars/lexus-es250-ext-3.jpg',
+        '/images/cars/lexus-es250-ext-4.jpg',
+        '/images/cars/lexus-es250-ext-5.jpg'
+      ],
+      interior: [
+        '/images/cars/lexus-es250-int-1.jpg',
+        '/images/cars/lexus-es250-int-2.jpg',
+        '/images/cars/lexus-es250-int-3.jpg',
+        '/images/cars/lexus-es250-int-4.jpg',
+        '/images/cars/lexus-es250-int-5.jpg',
+        '/images/cars/lexus-es250-int-6.jpg'
+      ],
+      details: []
+    },
+    specs: []
+  },
+  {
+    id: 'cadillac-escalade-sport-2025',
+    name: 'Cadillac Escalade Sport',
+    nameAr: 'كاديلاك إسكاليد سبورت',
+    brand: 'Cadillac',
+    year: 2025,
+    price: 465000,
+    status: 'available',
+    color: 'فضي',
+    engine: '6.2L V8',
+    transmission: '10-speed automatic',
+    drivetrain: '4WD',
+    harajUrl: 'https://haraj.com.sa/11187663468?src=android',
+    heroImage: '/images/cars/cadillac-escalade-hero.jpg',
+    gallery: {
+      exterior: ['/images/cars/cadillac-escalade-ext-1.jpg'],
+      interior: ['/images/cars/cadillac-escalade-int-1.jpg'],
+      details: []
+    },
+    specs: []
+  }
+];
+
+export const getCarById = (id: string): Car | undefined => {
+  return cars.find(car => car.id === id);
+};
+
+export const getAvailableCars = (): Car[] => {
+  return cars.filter(car => 
+    car.status === 'available' || 
+    car.status === 'جديدة' || 
+    car.status === 'بطاقة جمركية'
+  );
+};
+
+export const getCarsByBrand = (brand: string): Car[] => {
+  return cars.filter(car => car.brand.toLowerCase() === brand.toLowerCase());
+};
